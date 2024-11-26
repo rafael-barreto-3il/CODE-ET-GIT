50b74f9589c2b006a3589e3b16eefb68714ee412
barreto <barretopanr@3il.fr>
2024-11-26 17:35:03 +0100

    Commit de l'exercice 1 modifié

--- exercice_1/exercice1.java
+++ exercice_1/exercice1.java
@@ -1,16 +1,27 @@
 public class FactureManager {
+    private String typeProduit;
+    private int quantite;
+    private double prixUnitaire;
 
-    // Calcul le total d'une facture
+    /**
+     * Fonction qui permet de calculer le total de la facture
+     * 
+     *@param typeProduit : permet d'avoir le type du produit
+     *@param quantite : permet d'obtenir la quantite du produit
+     *@param prixUnitaire : correspond au prix du produit à l'unité
+     *@return retourne le total après application des réductions sur le produit
+     */
     public double calculerFacture(String typeProduit, int quantite, double prixUnitaire) {
         double total = quantite * prixUnitaire;
 
         // reduction selon la catégorie
-        if (typeProduit.equals("Alimentaire")) {
-            total -= total * 0.05; // Réduction de 5%
-        } else if (typeProduit.equals("Electronique")) {
-            total -= total * 0.1; // Réduction de 10%
-        } else if (typeProduit.equals("Luxe")) {
-            total -= total * 0.15; // Réduction de 15%
+        switch(typeProduit){
+            case "Alimentaire": total -= total * 0.05; // Réduction de 5%
+            break;
+            case "Electronique": total -= total * 0.1; // Réduction de 10%
+            break;
+            case "Luxe": total -= total * 0.15; // Réduction de 15%
+            break;
         }
 
         // Reduction sur le total
